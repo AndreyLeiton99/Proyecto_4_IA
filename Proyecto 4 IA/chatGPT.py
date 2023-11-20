cd24dc670ea155e6384d4e54b07a338bd8ff84a8
AndreyLeiton99 <aleiton2014@hotmail.com>
2023-11-20 00:46:58 -0600

    JSON files as knowledge base
    JSON files now read first before the request for the question goes to the API.
    If the answer exists inside JSON options, it won´t be necesarry to make the request to the API.

--- Proyecto 4 IA/chatGPT.py
+++ Proyecto 4 IA/chatGPT.py
@@ -1,5 +1,8 @@
 import openai
 import time
+import json
+
+
 # OpenAI charges you for the prompt and the output text at a rate of $0.002/1000 tokens
 # which is roughly 750 words.
 
@@ -12,29 +15,37 @@ import time
 
 # Chats, historial con el nombre/titulo de chats o algun identificador del chat
 # Interfaz grafica?
-# 
 
 class ChatGPT:
     def __init__(self, api_key, model="gpt-3.5-turbo"):
         openai.api_key = api_key
         self.model = model
         self.chat_history = []
+        self.user_input = ""
 
     def prompt_user(self):
-        user_input = input("You: ")
-        return user_input
+        self.user_input = input("You: ")
+        return self.user_input
 
     def add_user_message(self, message):
         self.chat_history.append({"role": "user", "content": message})
 
     def generate_response(self):
+        question = self.user_input
+
+        with open("knowledge_base.json", "r") as f:
+            knowledge_base = json.load(f)
+
+        for question_item in knowledge_base["questions"]:
+            if question_item["question"] == question:
+                return question_item["answer"]
+
         response = openai.ChatCompletion.create(
             model=self.model,
             messages=self.chat_history,
             max_tokens=150,
         )
-
-        return response['choices'][0]['message']['content'] if 'choices' in response and response['choices'] else ''
+        return response['choices'][0]['message']['content'] if 'choices' in response and response['choices'] else 'Lo sentimos, no podemos contestar a tu pregunta en este momento'
 
     def chat_loop(self):
         while True:
@@ -53,3 +64,23 @@ class ChatGPT:
 
             print(f"GPT: {assistant_reply}")
             self.add_user_message(assistant_reply)
+
+# Load the knowledge base from the .txt file
+# with open('knowledge_base.txt', 'r') as knowledge_base_file:
+#     knowledge_base = knowledge_base_file.read()
+#
+# # Split the knowledge base into a list of question-answer pairs
+# knowledge_base_pairs = knowledge_base.split('\n')
+#
+# # Search the knowledge base for a match to the user's question
+# for pair in knowledge_base_pairs:
+#     question, answer = pair.split('?')
+#     if question.lower() == user_input.lower():
+#         return answer
+#
+# # If no match is found, use the OpenAI API to generate a response
+# return openai.ChatCompletion.create(
+#     model=self.model,
+#     messages=self.chat_history,
+#     max_tokens=150,
+# )
--- Proyecto 4 IA/main.py
+++ Proyecto 4 IA/main.py
@@ -1,6 +1,6 @@
 from chatGPT import ChatGPT
 
 if __name__ == "__main__":
-    api_key = "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"
+    api_key = "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"
     chatbot = ChatGPT(api_key)
     chatbot.chat_loop()
